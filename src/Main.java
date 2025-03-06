644d7f6c24df89d654551eb808acdf820d9e6ae2
SaharRamezani <sahar.ramezani.jolfaei@gmail.com>
2025-03-06 12:55:25 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(20);
+        // Rectangle rectangle = new Rectangle(4, 6);
+        System.out.println("Area: " + circle.area());
+        System.out.println("Perimeter: " + circle.perimeter());
+    }
+    
+}
